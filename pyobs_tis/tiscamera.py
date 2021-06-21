5e84712358e08a58bd5752073ad3065c00d9f642
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-06-21 09:55:20 +0200

    image format

--- pyobs_tis/tiscamera.py
+++ pyobs_tis/tiscamera.py
@@ -8,23 +8,37 @@ log = logging.getLogger(__name__)
 
 
 class TisCamera(BaseWebcam):
-    def __init__(self, device: str, format: str = '', *args, **kwargs):
+    def __init__(self, device: str, format: str, *args, **kwargs):
         BaseWebcam.__init__(self, *args, **kwargs)
 
         # store
         self._device = device
         self._format = format
         self._camera = None
+        self._last_image_time = None
 
     def open(self):
-        """Open module"""p
+        """Open module"""
         BaseWebcam.open(self)
 
-        # open camera
+        # create camera
         self._camera = TIS.TIS()
-        self._camera.openDevice(self._device, 1280, 960, "15/1", TIS.SinkFormats.GRAY8, False)
+        self._camera.serialnumber = self._device
+
+        # get formats
+        formats = self._camera.createFormats()
+        if self._format not in formats:
+            raise ValueError('Invalid format: %s' % self._format)
+        fmt = formats[self._format]
+
+        # resolution and fps
+        res = fmt.res_list[0]
+        fps = res.fps[0]
+
+        # open camera
+        log.info('Opening webcam with %dx%d at %s fps.', res.width, res.height, fps)
+        self._camera.openDevice(self._device, res.width, res.height, fps, TIS.SinkFormats.GRAY8, False)
         self._camera.Set_Image_Callback(self.new_image)
-        self._camera.Set_Property("Exposure", 1000)
 
         # start taking images
         if not self._camera.Start_pipeline():
@@ -37,21 +51,14 @@ class TisCamera(BaseWebcam):
         # stop live video stream
         self._camera.Stop_pipeline()
 
-    def wait_for_frame(self, *args, **kwargs):
-        pass
-
-    def get_last_frame(self, *args, **kwargs) -> str:
-        pass
-
     def new_image(self, tis):
-        interval = 0.5
-        if self.last_image is not None and time.time() < self.last_image + interval:
+        if self._last_image_time is not None and time.time() < self._last_image_time + self._interval:
             return
-        self.last_image = time.time()
+        self._last_image_time = time.time()
 
         # get image and process it
-        img = self.camera.Get_image()
-        self._set_image(img)
+        img = self._camera.Get_image()
+        self._set_image(img[:, :, 0])
 
 
 __all__ = ['TisCamera']
